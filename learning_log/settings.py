5879871d6757558e2fa327841e541bedb7eef7f3
FarazPC <faraznqidwai@gmail.com>
2026-01-03 03:38:01 -0800

    Superuser

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+    #My Apps
+    'learning_logs',
+
+    #Default Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
---
+++ learning_logs/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """Topic the user is learning"""
+
+    text = models.CharField(max_length = 200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+    
+
+class Entry(models.Model):
+    """What did they exactly learn"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+
+        return f"{self.text[:50]}..."
