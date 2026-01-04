4826ed87722eee5e88e657767a649f97abca90d4
FarazPC <faraznqidwai@gmail.com>
2026-01-04 02:22:04 -0800

    I did something

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Topic(models.Model):
     """Topic the user is learning"""
 
-    text = models.CharField(max_length = 200)
+    text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
-    
+
 
 class Entry(models.Model):
     """What did they exactly learn"""
@@ -23,5 +23,4 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a string representation of the model."""
-
         return f"{self.text[:50]}..."
